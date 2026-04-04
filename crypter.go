ff1a6f2a86710421102595a943a77b1c3cfae77f
Ali <bincyber@users.noreply.github.com>
2026-04-04 08:23:54 +0400

    fix: nil crypter initialization panic

--- crypter.go
+++ crypter.go
@@ -30,11 +30,19 @@ func Init(c Crypterer) {
 // Encrypt reads plaintext from an io.Reader
 // and writes ciphertext to an io.Writer.
 func Encrypt(w io.Writer, r io.Reader) error {
+	if crypter == nil {
+		return ErrCrypterNotInitialized
+	}
+
 	return crypter.Encrypt(w, r)
 }
 
 // Decrypt reads ciphertext from an io.Reader
 // and writes plaintext to an io.Writer.
 func Decrypt(w io.Writer, r io.Reader) error {
+	if crypter == nil {
+		return ErrCrypterNotInitialized
+	}
+
 	return crypter.Decrypt(w, r)
 }
